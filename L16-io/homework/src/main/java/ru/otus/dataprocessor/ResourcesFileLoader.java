240cdb2003be3fd36383bc47d9abba4b655e1400
parfenov <e.parfenov@cft.ru>
2023-06-10 22:45:01 +0700

    hw16

--- L16-io/homework/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ L16-io/homework/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,16 +1,34 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import ru.otus.model.Measurement;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ResourcesFileLoader implements Loader {
 
+    private final String fileName;
+    private final Gson gson = new Gson();
     public ResourcesFileLoader(String fileName) {
+        this.fileName = fileName;
     }
 
     @Override
     public List<Measurement> load() {
-        //читает файл, парсит и возвращает результат
-        return null;
+       try(FileReader reader = new FileReader(fileName)) {
+           Type founderListType = new TypeToken<ArrayList<Measurement>>(){}.getType();
+            List<Measurement>  measurements = gson.fromJson(reader, founderListType);
+            return measurements;
+        } catch (Exception e) {
+           throw new FileProcessException(e);
+        }
     }
 }
