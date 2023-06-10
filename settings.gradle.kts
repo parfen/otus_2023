de0a1a253d571615a8d68b012414576586d7f028
parfenov <e.parfenov@cft.ru>
2023-06-10 18:17:42 +0700

    Init hw16

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,12 +9,16 @@ include ("L12-solid")
 include("L15-structuralPatterns:demo")
 include("L15-structuralPatterns:homework")
 
+include("L16-io:demo")
+include("L16-io:homework")
+
 pluginManagement {
     val jgitver: String by settings
     val dependencyManagement: String by settings
     val springframeworkBoot: String by settings
     val johnrengelmanShadow: String by settings
     val jib: String by settings
+    val protobufVer: String by settings
 
     plugins {
         id("fr.brouillard.oss.gradle.jgitver") version jgitver
@@ -22,5 +26,6 @@ pluginManagement {
         id("org.springframework.boot") version springframeworkBoot
         id("com.github.johnrengelman.shadow") version johnrengelmanShadow
         id("com.google.cloud.tools.jib") version jib
+        id("com.google.protobuf") version protobufVer
     }
 }
