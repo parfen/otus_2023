aca27cc641867da06645515ca70d1aeebd5d687d
parfenov <e.parfenov@cft.ru>
2023-08-07 18:47:32 +0700

    HW22 mycache

--- L22-cache/src/main/java/ru/otus/cachehw/MyCache.java
+++ L22-cache/src/main/java/ru/otus/cachehw/MyCache.java
@@ -1,31 +1,47 @@
 package ru.otus.cachehw;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
+
 public class MyCache<K, V> implements HwCache<K, V> {
-//Надо реализовать эти методы
+
+    private final Map<K,V> weakHashMap = new WeakHashMap<>();
+    private final List<HwListener<K, V>> listeners= new ArrayList<>();
 
     @Override
     public void put(K key, V value) {
-
+        weakHashMap.put(key,value);
+        sendActionListeners(key, value,"put");
     }
 
     @Override
     public void remove(K key) {
-
+        sendActionListeners(key, weakHashMap.get(key),"remove");
+        weakHashMap.remove(key);
     }
 
     @Override
     public V get(K key) {
-        return null;
+        sendActionListeners(key, weakHashMap.get(key),"get");
+        return weakHashMap.get(key);
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-
+        listeners.add(listener);
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
+        listeners.remove(listener);
+    }
 
+    private void sendActionListeners(K key, V value, String action) {
+        for(var listener : listeners) {
+           listener.notify(key, value, action);
+        }
     }
 }
