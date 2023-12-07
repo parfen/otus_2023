65fa5f9985b6cdfced82b6f43e7578237d685a13
parfen <parfenovevgeniy@mail.ru>
2023-12-07 22:31:43 +0700

    HW24

--- L21-jpql/homework-template/build.gradle.kts
+++ L21-jpql/homework-template/build.gradle.kts
@@ -8,6 +8,25 @@ dependencies {
 
     implementation("org.postgresql:postgresql")
 
+
+    implementation("ch.qos.logback:logback-classic")
+    implementation("com.google.code.gson:gson")
+
+    implementation("org.eclipse.jetty:jetty-servlet")
+    implementation("org.eclipse.jetty:jetty-server")
+    implementation("org.eclipse.jetty:jetty-webapp")
+    implementation("org.eclipse.jetty:jetty-security")
+    implementation("org.eclipse.jetty:jetty-http")
+    implementation("org.eclipse.jetty:jetty-io")
+    implementation("org.eclipse.jetty:jetty-util")
+    implementation("org.freemarker:freemarker")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
+    testImplementation("org.assertj:assertj-core")
+    testImplementation("org.mockito:mockito-junit-jupiter")
+
+
     testImplementation("com.h2database:h2")
     testImplementation("org.junit.jupiter:junit-jupiter-engine")
     testImplementation("org.junit.jupiter:junit-jupiter-params")
